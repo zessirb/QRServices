d44794f6fac35c9d2b76ad5f1b70dd934e3764d5
Ozymandias <ozymandias@localhost.localdomain>
2017-05-31 10:11:33 +0200

    Création de deux Servlet.  Ajout des dépendances Maven pour Hibernate.

---
+++ src/main/java/servlet/ManagerServlet.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet de gestion des services d'un utilisateur depuis un manager.
+ * D'ici, l'utilisateur peut créer, modifier et supprimer des services lui appartenant.
+ */
+@WebServlet(name="Manager", urlPatterns="/manager")
+public class ManagerServlet extends HttpServlet {
+	private static final long serialVersionUID = 8802864716415469670L;
+
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+    	response.setContentType("text/html");
+        response.setCharacterEncoding( "UTF-8" );
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html><html><body><h1>Manager</h1></body></html>");
+    }
+}
---
+++ src/main/java/servlet/ServiceServlet.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet d'interactions avec des services existants
+ * D'ici, l'utilisateur peut utiliser un service existant
+ */
+@WebServlet(name="Manager", urlPatterns="/manager")
+public class ServiceServlet extends HttpServlet {
+	private static final long serialVersionUID = 4744540075863839040L;
+
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+    	response.setContentType("text/html");
+        response.setCharacterEncoding( "UTF-8" );
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html><html><body><h1>Manager</h1></body></html>");
+    }
+}
